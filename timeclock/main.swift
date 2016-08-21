e6b577eac16c326aed0ba62eaa6c1a1a1fa7a03d
Jonathan Warner <calignosus@gmail.com>
2016-08-22 09:14:41 +1000

    Initial Commit

---
+++ timeclock/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  timeclock
+//
+//  Created by Jonathan Warner on 22/08/2016.
+//  Copyright © 2016 Jonathan Warner. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
